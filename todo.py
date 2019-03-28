ceb10a24eaedd146ec972bbd40409cd3125cd71a
Aríel Jóhann Árnason <ariel17@ru.is>
2019-03-28 15:23:28 +0000

    dfj

---
+++ todo.py
@@ -0,0 +1,45 @@
+TODO:
+
+- Tengja sport objects við lista af notendum
+- Tengja sport við groups
+
+- prenta lista af sport objects og str: 
+name of sport
+ -- member name + age
+ -- member name + age
+ -- member name + age
+ -- member name + age
+
+- Edit member: remove from sport - remove from group
+
+- Groups:
+User shall be able to register groups in each sport
+    Variables:
+    --Group name
+    --Age range
+
+    --When signing a user up for a sport a group must be selected
+    --Only groups for correct age available
+    --Possible to sign into more than one group, if age allows
+    --When viewing details for a sport the user 
+    should see a list of groups
+
+    --User should be able to select a group and see details
+    --List of users in a group
+    --When viewing details on a user the sport and 
+    --group should be displayed in list
+
+
+- Undo functionality:
+The system shall keep track of every operation done in the system
+○ At least operations that change data
+● The user shall be able to undo the last operation
+○ Then the next-to-last, etc… like undo usually works
+
+
+- Waiting list functionality:
+The user shall be able to register a maximum number of members in a group
+● If a group is full a member is added to waiting list instead
+● If member is removed from a full group a member from the waiting list is added instead
+○ Notify the user when this happens
+
